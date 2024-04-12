5652f8fb99ea57cdbf36ce57debe5a963c338229
iZarrios <eyad_zarrios2001@yahoo.com>
2024-04-12 22:42:54 +0200

    update: removing let & return stms parsing TODOs

--- parser/parser.go
+++ parser/parser.go
@@ -96,22 +96,22 @@ func (p *Parser) parseStatement() ast.Statement {
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
-
 	if !p.expectPeek(token.IDENT) {
 		return nil
 	}
-
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
-
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
 
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
-	for p.curToken.Type != token.SEMICOLON {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekToken.Type == token.SEMICOLON {
 		p.nextToken()
 	}
+
 	return stmt
 }
 
@@ -120,8 +120,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
 	for p.curToken.Type != token.SEMICOLON {
 		p.nextToken()
 	}
