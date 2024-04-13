27957c4329a254656dd8bfc6506b661abe977a25
iZarrios <eyad_zarrios2001@yahoo.com>
2024-04-13 10:52:43 +0200

    update:  add braces in switch stms

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -9,52 +9,77 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	switch node := node.(type) {
 	// Statements
 	case *ast.Program:
-		return evalProgram(node, env)
+		{
+			return evalProgram(node, env)
+		}
 	case *ast.ExpressionStatement:
-		return Eval(node.Expression, env)
+		{
+			return Eval(node.Expression, env)
+		}
 	// Expressions
 	case *ast.IntegerLiteral:
-		return &object.Integer{Value: node.Value}
+		{
+			return &object.Integer{Value: node.Value}
+		}
 	case *ast.Boolean:
-		return nativeBoolToBooleanObject(node.Value)
+		{
+			return nativeBoolToBooleanObject(node.Value)
+		}
 	case *ast.PrefixExpression:
-		right := Eval(node.Right, env)
-		if isError(right) {
-			return right
-		}
+		{
+			right := Eval(node.Right, env)
+			if isError(right) {
+				return right
+			}
 
-		return evalPrefixExpression(node.Operator, right)
+			return evalPrefixExpression(node.Operator, right)
+		}
 	case *ast.InfixExpression:
-		left := Eval(node.Left, env)
-		if isError(left) {
-			return left
-		}
+		{
+			left := Eval(node.Left, env)
+			if isError(left) {
+				return left
+			}
 
-		right := Eval(node.Right, env)
-		if isError(right) {
-			return right
-		}
+			right := Eval(node.Right, env)
+			if isError(right) {
+				return right
+			}
 
-		return evalInfixExpression(node.Operator, left, right)
+			return evalInfixExpression(node.Operator, left, right)
+		}
 	case *ast.BlockStatement:
-		return evalBlockStatement(node, env)
-	case *ast.IfExpression:
-		return evalIfExpression(node, env)
-	case *ast.ReturnStatement:
-		val := Eval(node.ReturnValue, env)
-		if isError(val) {
-			return val
-		}
+		{
 
-		return &object.ReturnValue{Value: val}
-	case *ast.LetStatement:
-		val := Eval(node.Value, env)
-		if isError(val) {
-			return val
+			return evalBlockStatement(node, env)
 		}
-		env.Set(node.Name.Value, val)
+	case *ast.IfExpression:
+		{
+
+			return evalIfExpression(node, env)
+		}
+	case *ast.ReturnStatement:
+		{
+
+			val := Eval(node.ReturnValue, env)
+			if isError(val) {
+				return val
+			}
+
+			return &object.ReturnValue{Value: val}
+		}
+	case *ast.LetStatement:
+		{
+			val := Eval(node.Value, env)
+			if isError(val) {
+				return val
+			}
+			env.Set(node.Name.Value, val)
+		}
 	case *ast.Identifier:
-		return evalIdentifier(node, env)
+		{
+			return evalIdentifier(node, env)
+		}
 	}
 	return nil
 }
