057eecf514f6a358b45f61e681e7081c0d0f996e
iZarrios <eyad_zarrios2001@yahoo.com>
2024-04-15 06:30:55 +0200

    update: hasing

--- parser/parser.go
+++ parser/parser.go
@@ -47,6 +47,7 @@ func NewParser(l *lexer.Lexer) (*Parser, error) {
 		p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 		p.registerPrefix(token.STRING, p.parseStringLiteral)
 		p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+		p.registerPrefix(token.LBRACE, p.parseHashLiteral)
 	}
 
 	{ // INFIX
@@ -376,4 +377,26 @@ func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
 	}
 
 	return exp
+}
+
+func (p *Parser) parseHashLiteral() ast.Expression {
+	hash := &ast.HashLiteral{Token: p.curToken}
+	hash.Pairs = make(map[ast.Expression]ast.Expression)
+	for p.peekToken.Type != token.RBRACE {
+		p.nextToken()
+		key := p.parseExpression(LOWEST)
+		if !p.expectPeek(token.COLON) {
+			return nil
+		}
+		p.nextToken()
+		value := p.parseExpression(LOWEST)
+		hash.Pairs[key] = value
+		if p.peekToken.Type != token.RBRACE && !p.expectPeek(token.COMMA) {
+			return nil
+		}
+	}
+	if !p.expectPeek(token.RBRACE) {
+		return nil
+	}
+	return hash
 }
